2ef70f5ccd47755947f7a6950eb62394d90ad82e
protoblaster <protoblaster11@protonmail.com>
2024-12-10 23:38:11 +0000

    4th-commit -- mod up.sh new func and code

--- up.sh
+++ up.sh
@@ -13,11 +13,21 @@ commit_git() {
 	git push -u origin main
 }
 
+exit_successful() {
+	echo "Commit was successful... Ending script"
+	sleep 2
+	exit
+}
+
 read -p "Would you liek to upload changes to git?: " ans
 
 if [ $ans == "y" ]; then
 	commit_git
-	exit_gracefully
+	if [ $? -eq 0 ]; then
+		exit_successful
+	else
+		exit_gracefully
+	fi
 else
 	exit_gracefully
 fi
